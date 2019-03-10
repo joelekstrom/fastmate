f9e5bc0c04493e0d455effa1aa6f071114055baf
Joel Ekström <joel@ekstrom.io>
2019-03-10 08:22:08 +0100

    Store and restore window size/location
    Previously, if the user had enabled the "Close windows when quitting an
    application"-setting, this made the app have default size on
    every launch.
    
    Fixes #4

--- Fastmate/MainWindowController.m
+++ Fastmate/MainWindowController.m
@@ -6,9 +6,24 @@
 
 @implementation MainWindowController
 
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    NSString *lastWindowFrame = [NSUserDefaults.standardUserDefaults objectForKey:@"mainWindowFrame"];
+    if (lastWindowFrame) {
+        NSRect frame = NSRectFromString(lastWindowFrame);
+        [self.window setFrame:frame display:NO];
+    }
+}
+
 - (BOOL)windowShouldClose:(NSWindow *)sender {
     [NSApp hide:sender];
     return NO;
+}
+
+- (void)windowDidResize:(NSNotification *)notification {
+    if (self.windowLoaded) {
+        [NSUserDefaults.standardUserDefaults setObject:NSStringFromRect(self.window.frame) forKey:@"mainWindowFrame"];
+    }
 }
 
 @end
