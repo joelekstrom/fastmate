cdd090a38787ba71ffa4b21faa04d584379f94f8
Joel <joel@ekstrom.dev>
2021-03-13 14:08:57 +0100

    Update print style to look a bit more like Safari
    Changes margins and adds headers and footers with title, date and page count
    
    Related to #31

--- Fastmate/PrintManager.m
+++ Fastmate/PrintManager.m
@@ -1,11 +1,14 @@
 #import "PrintManager.h"
 @import WebKit;
 
-@interface PrintManager() <WebFrameLoadDelegate>
+@interface PrintManager() <WebFrameLoadDelegate, WebUIDelegate>
 
 @property (nonatomic, strong) WebView *webView;
 @property (nonatomic, strong) NSPrintInfo *printInfo;
+@property (nonatomic, copy) NSString *headerTitle;
 @property (nonatomic, copy) NSString *emailTitle;
+@property (nonatomic, copy) NSURL *emailURL;
+@property (nonatomic, weak) NSPrintOperation *currentOperation;
 
 @end
 
@@ -23,10 +26,11 @@
 - (instancetype)init {
     if (self = [super init]) {
         _printInfo = [NSPrintInfo sharedPrintInfo];
-        _printInfo.topMargin = 25;
-        _printInfo.bottomMargin = 10;
-        _printInfo.rightMargin = 10;
-        _printInfo.leftMargin = 10;
+        _printInfo.topMargin = 0;
+        _printInfo.bottomMargin = 0;
+        _printInfo.rightMargin = 20;
+        _printInfo.leftMargin = 20;
+        _printInfo.printSettings[NSPrintHeaderAndFooter] = @(YES);
     }
     return self;
 }
@@ -36,6 +40,9 @@
     self.webView = [[WebView alloc] initWithFrame:webViewFrame frameName:@"printFrame" groupName:@"printGroup"];
     self.webView.shouldUpdateWhileOffscreen = true;
     self.webView.frameLoadDelegate = self;
+    self.webView.UIDelegate = self;
+    self.headerTitle = sourceView.title;
+    self.emailURL = sourceView.URL;
 
     // Get e-mail title to set default name of PDF
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"– (.*) \\| Fastmail" options:0 error:nil];
@@ -60,12 +67,54 @@
         NSWindow *window = NSApp.mainWindow ?: NSApp.windows.firstObject;
         NSPrintOperation *printOperation = [NSPrintOperation printOperationWithView:frame.frameView.documentView printInfo:self.printInfo];
         printOperation.jobTitle = self.emailTitle;
+        self.currentOperation = printOperation;
         [printOperation runOperationModalForWindow:window delegate:self didRunSelector:@selector(printOperationDidFinish) contextInfo:nil];
     }
 }
 
 - (void)printOperationDidFinish {
     self.webView = nil;
+}
+
+- (float)webViewHeaderHeight:(WebView *)sender
+{
+    return 50.0;
+}
+
+- (void)webView:(WebView *)sender drawHeaderInRect:(NSRect)rect
+{
+    CGFloat verticalOffset = 20.0;
+    NSDictionary *fontAttributes = @{NSFontAttributeName: [NSFont systemFontOfSize:7.0]};
+
+    [self.headerTitle drawWithRect:CGRectOffset(rect, 0, verticalOffset) options:0 attributes:fontAttributes];
+
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateStyle = NSDateFormatterShortStyle;
+    dateFormatter.timeStyle = NSDateFormatterMediumStyle;
+    NSString *dateString = [dateFormatter stringFromDate:NSDate.date];
+
+    CGSize dateSize = [dateString sizeWithAttributes:fontAttributes];
+    CGRect dateRect = CGRectMake(CGRectGetMaxX(rect) - dateSize.width, rect.origin.y + verticalOffset, dateSize.width, dateSize.height);
+
+    [dateString drawWithRect:dateRect options:0 attributes:fontAttributes];
+}
+
+- (float)webViewFooterHeight:(WebView *)sender
+{
+    return 50.0;
+}
+
+- (void)webView:(WebView *)sender drawFooterInRect:(NSRect)rect
+{
+    CGFloat verticalOffset = 20.0;
+    NSDictionary *fontAttributes = @{NSFontAttributeName: [NSFont systemFontOfSize:7.0]};
+    [self.emailURL.absoluteString drawWithRect:CGRectOffset(rect, 0, verticalOffset) options:0 attributes:fontAttributes];
+
+    NSString *currentPageString = [NSString stringWithFormat:@"Page %@ of %@", @(self.currentOperation.currentPage), @(self.currentOperation.pageRange.length)];
+    CGSize pageSize = [currentPageString sizeWithAttributes:fontAttributes];
+    CGRect pageRect = CGRectMake(CGRectGetMaxX(rect) - pageSize.width, rect.origin.y + verticalOffset, pageSize.width, pageSize.height);
+
+    [currentPageString drawWithRect:pageRect options:0 attributes:fontAttributes];
 }
 
 @end
