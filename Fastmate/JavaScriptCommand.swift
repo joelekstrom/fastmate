8d6f2bf0b15e44a9d649463eaccd8a984c945c7a
Joel Ekström <joel@ekstrom.dev>
2022-06-10 10:53:36 +0200

    Remove unneeded downcast

--- Fastmate/JavaScriptCommand.swift
+++ Fastmate/JavaScriptCommand.swift
@@ -9,17 +9,13 @@ import Foundation
 
 class JavaScriptCommand : NSScriptCommand {
     override func performDefaultImplementation() -> Any? {
-        
         if let directParameter = directParameter as? String {
             let appDelegate = NSApplication.shared.delegate as? AppDelegate
-            let mainWebViewController = appDelegate?.mainWebViewController as? WebViewController
-            mainWebViewController?.webView?.evaluateJavaScript(directParameter)
+            appDelegate?.mainWebViewController?.webView?.evaluateJavaScript(directParameter)
         } else {
             self.scriptErrorNumber = -50;
             self.scriptErrorString = "Provide a JavaScript string to evaluate"
         }
-        
         return nil
-
     }
 }
